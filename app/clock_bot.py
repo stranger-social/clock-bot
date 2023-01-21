74158d81a9969cb6b32b921c949aad60408878be
AZcoigreach <azcoigreach@gmail.com>
2023-01-21 11:52:04 -0700

    20230121_1151

--- app/clock_bot.py
+++ app/clock_bot.py
@@ -1,3 +1,5 @@
+import os 
+
 from .config import settings
 from .database import get_db
 from . import models, schemas
@@ -8,8 +10,7 @@ import asyncio
 import aiohttp
 import json
 import croniter
-# from pytz import timezone
-
+import magic
 
 import logging
 
@@ -79,25 +80,42 @@ async def update_next_run(post):
 async def post_status(post):
     try:
         logger.debug(f"post_status: Checking post {post.id} status")
+        bot_token = await get_bot_token(post.bot_token_id)
         if post.bot_token_id == None:
             logger.error(f"post_status: Post {post.id} bot_token_id is Null")
             return False
         else:
-            bot_token = await get_bot_token(post.bot_token_id)
             if bot_token == None:
                 logger.error(f"post_status: Post {post.id} bot_token is Null")
                 return False
             else:
+                # Check for media_path and upload media if it exists
+                if post.media_path != None:
+                    logger.debug(f"post_status: Post {post.id} has media_path.  Uploading media")
+                    media_id = await upload_media(post)
+                    # add media_id to media_ids list
+                    if media_id == None:
+                        logger.error(f"post_status: Post {post.id} media_id returned Null")
+                        media_ids = None
+                    else:
+                        media_ids = [media_id]
+                        logger.debug(f"post_status: Post {post.id} media_ids: {media_ids}")
+                else:
+                    media_ids = None
+                    logger.debug(f"post_status: Post {post.id} has no media_path.  Not uploading media")
+                # Post status
                 headers = {
                 "Authorization": f"Bearer {bot_token}",
-                "Content-Type": "application/json"
                 }
+                # Conver payload to json
                 data = json.dumps({
                     "status": post.content,
                     "sensitive": post.sensitive,
                     "spoiler_text": post.spoiler_text,
                     "visibility": post.visibility,
+                    "media_ids": media_ids
                 })
+                logger.debug(f"post_status: Post {post.id} data: {data}")
                 if quiet_mode() == True:
                     logger.warning(f"post_status: Quiet mode is enabled.  Status {post.id} not posted")
                     # Update post_log table with post_id and last_posted
@@ -111,7 +129,13 @@ async def post_status(post):
                                 await update_next_run(post)
                                 await update_post_log(post)
                                 logger.info(f"post_status: Status {post.id} posted successfully at {datetime.now()}")
+                                logger.debug(f"post_status: Status {post.id} response: {await response.json()}")
                                 return True
+                            else:
+                                await update_next_run(post)
+                                logger.error(f"post_status: Status {post.id} not posted.  Response status: {response.status}")
+                                logger.debug(f"post_status: Status {post.id} response: {await response.json()}")
+                                return False
     except Exception as e:
         logger.error(f"post_status: {e}")
 
@@ -139,6 +163,46 @@ async def get_bot_token(bot_token_id):
     except Exception as e:
         logger.error(f"get_bot_token: {e}")
 
+# Use AIOHTTP to upload media and return media_id, include error handling and logging
+# POST /api/v2/media HTTP/1.1
+# https://docs.joinmastodon.org/methods/media/
+async def upload_media(post):
+    try:
+        logger.debug(f"upload_media: Uploading media for post {post.id} media_path {post.media_path}")
+        # with the media_path open check file has has a mime type and upload media
+        with open(post.media_path, "rb") as media:
+            media_mime_type = magic.from_buffer(media.read(1024), mime=True)
+            logger.debug(f"upload_media: Post {post.id} media_mime_type {media_mime_type}")
+            # reset file pointer to start of file
+            media.seek(0)
+            # get bot_token from post.bot_token_id
+            bot_token = await get_bot_token(post.bot_token_id)
+            # upload media
+            headers = {
+                "Authorization" : f"Bearer {bot_token}"
+            }
+            data = {
+                "file": media
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.post(f"{settings.mastodon_base_url}/api/v2/media", headers=headers, data=data) as response:
+                    if response.status == 200 or response.status == 202:
+                        # logger debug response
+                        logger.debug(f"upload_media: Response for post {post.id} media_path {post.media_path} media_mime_type {media_mime_type} {await response.text()}")
+                        # extract id from json response
+                        media_id = (await response.json())['id']
+                        logger.debug(f"upload_media: Media uploaded successfully for post {post.id} media_id {media_id}")
+                        return media_id
+                    elif response.status == 422:
+                        logger.error(f"upload_media: Error uploading media for post {post.id} media_path {post.media_path} media_mime_type {media_mime_type}")
+                        return None
+                    else:
+                        logger.error(f"upload_media: Error uploading media for post {post.id}")
+                        return None
+    except Exception as e:
+        logger.error(f"upload_media: {e}")
+
+
 async def clear_next_run():
     try:
         with get_db() as db:
@@ -155,12 +219,6 @@ async def clear_next_run():
 
 async def clock_bot_main():
     while True:
-        # Use croniter to check to generate a heartbeat '*/10 * * * *'
-        schedule = croniter.croniter('*/10 * * * *')
-        next_run = schedule.get_next(datetime)
-        logger.debug(f"clock_bot_main: Next run: {next_run}")
-        if next_run < datetime.now():
-            logger.info(f'clock_bot_main: Heartbeat at {datetime.now()}')
         await check_scheduled_posts()
         await asyncio.sleep(10)
        
